412713095a37e6ca22eda0b83ccf7b5949514607
George Daramouskas <george.daramouskas@gmail.com>
2017-07-16 01:27:07 +0200

    [IMP] Fetches all the data and inserts it on your local MongoDB

--- cargr.py
+++ cargr.py
@@ -1,42 +1,46 @@
 # -*- coding: utf-8 -*-
+import requests
+import json
+from pymongo import MongoClient
 
-import scrapy
-from scrapy.shell import inspect_response
-from scrapy.exporters import JsonLinesItemExporter as Exporter
-
-# TODO how to mine data from car.gr? What kind of info would one want ?
-# PRICE ALERT
-# car/spare part availability
-# monitor specific sellers, try to offer advice to the customer
-# how long does a ad stay up ?
-
-
-class CargrScrapper(scrapy.Spider):
-    name = 'cargr'
-
-    start_urls = ['https://www.car.gr/classifieds/cars/view/9172585/']
-
-    def start_requests(self):
-        request = scrapy.Request(self.start_urls[0] + '?lang=en') # TODO: how to pass params to url ?
-        request.headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'})
-        yield request
-
-    def parse(self, response):
-        # TODO: if ends with / put 1, else increment by 1
-        # if response.ok != True skip page
-        # extract the following
-        export_file = open('cars.json', 'wb')
-        exporter = Exporter(export_file)
-        car = {}
-        tag_tr_list = response.xpath("//table[1]//tr")
-        for tr in tag_tr_list:
-            _key = tr.xpath(".//td[1]/text()").extract()
-            _value = tr.xpath(".//td[2]/span/text()").extract()
-            #inspect_response(response, self)
-            if not _value:
-                _value = tr.xpath(".//td[2]/node()/text()").extract()
-            if _key and _value:
-                car.update({_key[0]: _value[0]})
-        exporter.export_item(car)# TODO: utf should be decoded
+VEHICLE_TYPES = ['bus', 'builder', 'boat', 'bicycle', 'van', 'truck'
+                 'semitruck', 'semitrailer', 'tractor', 'forklift',
+                 'trailer', 'taxi', 'motorhome', 'caravan', 'airsport',
+                 'radiocontrol', 'gokart', 'watersport', 'snowsport',
+                 'car', 'bike']
 
 
+def _import():
+    database = MongoClient().cargr
+    for vtype in VEHICLE_TYPES:
+        _collection = database[vtype]
+        page_no = 1
+        listings = []
+        while True:
+            print 'We are searching for ', vtype
+            print 'We are on page', page_no
+            response = requests.post(url='''
+            https://www.car.gr/mobile/search?&hash=db2a3ce6916ad24136314e335cf22e91&uuid=fd2c0ebe-4b5a-4a9a-b3c2-1e3676648664&fs=1
+            ''', data='vtype={}&pg={}'.format(vtype, page_no),
+            headers={'Authorization': 'Basic dGVzdDp0ZXN0',
+                     'Content-Type': 'application/x-www-form-urlencoded',
+                     'User-Agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; LG-D722 Build/KOT49I.A1415241157)',
+                     #'Host': 'ws.car.gr',
+                     'Connection': 'Keep-Alive',
+                     'Accept-Encoding': 'gzip'})
+            print response.content
+            content = json.loads(response.content)
+            count_page = len(content['response']['rows'])
+            rows = content['response']['rows']
+            for listing in rows.iterkeys():
+                listings.append(rows[listing])
+            page_no += 1
+            if count_page == 0:
+                break
+            print len(listings)
+        print 'Inserting', vtype, ' into our database'
+        _collection.insert_many(listings)
+
+
+if __name__ == '__main__':
+    _import()
